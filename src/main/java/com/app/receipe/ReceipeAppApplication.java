670d95c74b91477268c6a49db90fd4b8c7e29922
Shiva Krishnan <shivainfotech12@gmail.com>
2019-01-05 22:58:09 +0530

    Initial Commit

---
+++ src/main/java/com/app/receipe/ReceipeAppApplication.java
@@ -0,0 +1,14 @@
+package com.app.receipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceipeAppApplication.class, args);
+	}
+
+}
+
